42a9dd6290e0472aca2c981da52a9cceed7e7d82
ju-liechen <juliechen4649@gmail.com>
2024-01-04 09:53:27 -0400

    Remove input requirement from populate command for deployment

--- apps/product/management/commands/populate.py
+++ apps/product/management/commands/populate.py
@@ -10,15 +10,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         if User.objects.count() > 0:
-            confirm = input(
-                'Database has already been populated.'
-                'Do you want to reset the database and populate with new data?'
-                '[y/n] '
-            )
-            if confirm.lower() != 'y':
-                self.stdout.write('Database population cancelled.')
-                return
-
             depopulate()
             self.stdout.write(self.style.SUCCESS(
                 'Database has been reset. '
